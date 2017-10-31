c887992f166717594ad2211affa2a5779e970da6
Federico Pacheco <federicogpacheco@gmail.com>
2017-10-30 21:06:10 -0300

    convert deploy to war

--- dssd-backend/src/main/java/io/dssd/DssdBackendApplication.java
+++ dssd-backend/src/main/java/io/dssd/DssdBackendApplication.java
@@ -2,11 +2,19 @@ package io.dssd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
-@SpringBootApplication()
-public class DssdBackendApplication {
+@SpringBootApplication
+public class DssdBackendApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DssdBackendApplication.class);
+	}
+
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(DssdBackendApplication.class, args);
 	}
+
 }
